e4f6f32064dd871e9ff0bc5e5c61d66f2c0650d6
danymeza231-creator <danymeza231@gmail.com>
2026-02-07 10:45:13 -0600

    Update service-worker.js
    Fix: limpiar cache PWA (mapa en blanco)

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,18 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE_NAME = 'alarma-parada-v3';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
